690d22e6108ebc88c83c09ca1c9175af3c4a7c0b
sfivenine <112146630+sfivenine@users.noreply.github.com>
2023-05-14 17:06:34 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,108 @@
+﻿const generatedBibEntries = {
+    "7792228": {
+        "author": "Liu, Chien-Hung and Ting, Chuan-Kang",
+        "doi": "10.1109/TETCI.2016.2642200",
+        "journal": "IEEE Transactions on Emerging Topics in Computational Intelligence",
+        "number": "1",
+        "pages": "2-15",
+        "title": "Computational Intelligence in Music Composition: A Survey",
+        "type": "ARTICLE",
+        "volume": "1",
+        "year": "2017"
+    },
+    "8327197": {
+        "author": "Lopez-Rincon, Omar and Starostenko, Oleg and Mart\u00edn, Gerardo Ayala-San",
+        "booktitle": "2018 International Conference on Electronics, Communications and Computers (CONIELECOMP)",
+        "doi": "10.1109/CONIELECOMP.2018.8327197",
+        "number": "",
+        "pages": "187-193",
+        "title": "Algoritmic music composition based on artificial intelligence: A survey",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2018"
+    },
+    "8729266": {
+        "author": "Zhao, Kun and Li, Siqi and Cai, Juanjuan and Wang, Hui and Wang, Jingling",
+        "booktitle": "2019 IEEE 3rd Information Technology, Networking, Electronic and Automation Control Conference (ITNEC)",
+        "doi": "10.1109/ITNEC.2019.8729266",
+        "number": "",
+        "pages": "2039-2043",
+        "title": "An Emotional Symbolic Music Generation System based on LSTM Networks",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2019"
+    },
+    "8932479": {
+        "author": "Goienetxea, Izaro and Mendialdua, I\u00f1igo and Rodr\u00edguez, Igor and Sierra, Basilio",
+        "doi": "10.1109/ACCESS.2019.2959696",
+        "journal": "IEEE Access",
+        "number": "",
+        "pages": "183365-183382",
+        "title": "Statistics-Based Music Generation Approach Considering Both Rhythm and Melody Coherence",
+        "type": "ARTICLE",
+        "volume": "7",
+        "year": "2019"
+    },
+    "9301934": {
+        "author": "Huang, Chih-Fang and Huang, Cheng-Yuan",
+        "booktitle": "2020 IEEE Eurasia Conference on IOT, Communication and Engineering (ECICE)",
+        "doi": "10.1109/ECICE50847.2020.9301934",
+        "number": "",
+        "pages": "220-222",
+        "title": "Emotion-based AI Music Generation System with CVAE-GAN",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2020"
+    },
+    "9408813": {
+        "author": "Zeng, Zhen and Zhou, Li",
+        "booktitle": "2021 6th International Conference on Intelligent Computing and Signal Processing (ICSP)",
+        "doi": "10.1109/ICSP51882.2021.9408813",
+        "number": "",
+        "pages": "187-192",
+        "title": "A Memetic Algorithm for Chinese Traditional Music Composition",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2021"
+    },
+    "briot2020deep": {
+        "author": "Briot, Jean-Pierre and Pachet, Fran{\\c{c}}ois",
+        "journal": "Neural Computing and Applications",
+        "number": "4",
+        "pages": "981--993",
+        "publisher": "Springer",
+        "title": "Deep learning for music generation: challenges and directions",
+        "type": "article",
+        "volume": "32",
+        "year": "2020"
+    },
+    "briot2021artificial": {
+        "author": "Briot, Jean-Pierre",
+        "journal": "Neural Computing and Applications",
+        "number": "1",
+        "pages": "39--65",
+        "publisher": "Springer",
+        "title": "From artificial neural networks to deep learning for music generation: history, concepts and trends",
+        "type": "article",
+        "volume": "33",
+        "year": "2021"
+    },
+    "ji2020comprehensive": {
+        "archiveprefix": "arXiv",
+        "author": "Shulei Ji and Jing Luo and Xinyu Yang",
+        "eprint": "2011.06801",
+        "primaryclass": "cs.SD",
+        "title": "A Comprehensive Survey on Deep Music Generation: Multi-level Representations, Algorithms, Evaluations, and Future Directions",
+        "type": "misc",
+        "year": "2020"
+    },
+    "simon2018learning": {
+        "archiveprefix": "arXiv",
+        "author": "Ian Simon and Adam Roberts and Colin Raffel and Jesse Engel and Curtis Hawthorne and Douglas Eck",
+        "eprint": "1806.00195",
+        "primaryclass": "stat.ML",
+        "title": "Learning a Latent Space of Multitrack Measures",
+        "type": "misc",
+        "year": "2018"
+    }
+};
